74d2d1f6abdb0600d1ea14231bbd1e39fb343784
Naiim Taefy Jimenez <naiim.tj@gmail.com>
2024-09-03 04:40:50 +0200

    feat(b): Add Docker Compose and Dockerfile, small change code.

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      fastApiUri: process.env.FASTAPI_URI,
+      fastApiUri: process.env.FASTAPI_URI || 'http://localhost:5000',
     },
   },
 });
